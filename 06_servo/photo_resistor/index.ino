e27a8de21f72cf861b2b26981a5791c9bfa6e226
Manoj Rawat <manojrawat87634@gmail.com>
2026-05-30 13:05:13 +0530

    Update index.ino

--- 06_servo/photo_resistor/index.ino
+++ 06_servo/photo_resistor/index.ino
@@ -1,28 +1,22 @@
 #include <Servo.h>
+
 int lightPen = A0;
 int lightVal;
-int dv =250;
+int dv =2500;
 int outPin = 13;
-Servo myServo;
+Servo s;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(lightPen, INPUT);
   pinMode(outPin, OUTPUT);
-    myServo.attach(outPin);
-  Serial.begin(9600);
+  s.attach(outPin);
+  Serial.begin(9600); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   lightVal = analogRead(lightPen);
   Serial.println(lightVal);
-  myServo.write(1024 - lightVal);
-//   if (lightVal < 120){
-//     digitalWrite(outPin, HIGH);
-//   }
-//   else{
-//     digitalWrite(outPin, LOW);
-//   }
-  delay(dv);
+ s.write((180.0/1023.0) * lightVal);
 }
